c302d9f01bbdddd61a5de02ae698c5675dd12edf
Manuel Lopez <jmlopez.rod@gmail.com>
2014-02-19 17:38:25 -0600

    v0.0.2

--- log.py
+++ log.py
@@ -9,7 +9,7 @@ from lexor import init
 from lexor.core.writer import NodeWriter
 
 INFO = init(
-    version=(0, 0, 1, 'final', 1),
+    version=(0, 0, 2, 'final', 0),
     lang='lexor',
     type='writer',
     description='Display messages from parsed/converted documents.',
